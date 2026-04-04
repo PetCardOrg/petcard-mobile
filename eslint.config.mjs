b6d2740fb10099ee922f97207fded85e750abb24
Ricardo <123703671+ricardotemporal@users.noreply.github.com>
2026-04-04 01:43:50 +0000

    chore(setup): configura ESLint e Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', '.expo/'],
+  },
+];
